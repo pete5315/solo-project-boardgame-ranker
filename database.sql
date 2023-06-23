7a943afdd746ac94aeaf23b07a9992dc632004a4
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-23 10:02:25 -0500

    set up database and updated database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,16 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "lists" (
+	"id" SERIAL PRIMARY KEY,
+	"date" DATE,
+	"completed" BOOLEAN DEFAULT FALSE,
+	"decisions_array" VARCHAR[] null,
+	"data_array" VARCHAR[] null,
+	"games_array" VARCHAR[] null,
+	"matchups_array" VARCHAR[] null,
+	"user_id" INTEGER REFERENCES users
 );
